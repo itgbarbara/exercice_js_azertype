1716ca2196c84a71a6b5082870daaee89b5db162
itgbarbara <itgbarbara@gmail.com>
2024-04-29 14:56:13 +0200

    Exercice P2C3 boucles

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
 const listeMots = ["Cachalot","Pétunia","Serviette"]
+const listePhrases = ["Pas de panique !","La vie, l'univers et le reste !","Merci pour le poisson"]
+
 let score = 0
 
-// Déclaration de la variable contenant le mot saisi par l'utilisateur
-// Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
-let motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[0])
-
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-if (motUtilisateur === listeMots[0]) {
-    score += 1
-    console.log(score)
-} else {
-    console.log("Vous avez fait une erreur de frappe")
+// Déclaration de la variable contenant le choix de l'utilisateur
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
+//Tant que l'utilisateur n'a pas saisi "Mots" ou "Phrases", on lui redemande de saisir un choix
+while (choix != "Mots" && choix != "Phrases") {
+    console.log("Saisie incorrecte")
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
 }
 
-// Idem avec le second mot du tableau
-// Notez l'absence du "let" devant la déclaration de la variable "motUtilisateur", cela permet de réutiliser la variable déclarée précédemment
-motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score += 1
-    console.log(score)
+if (choix === "Mots") {
+    // On parcourt le tableau des mots
+    for (i=0; i < 3; i++) {
+        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+        let motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+            score++
+        }
+    }
 } else {
-    console.log("Vous avez fait une erreur de frappe")
+    for (i=0; i < 3; i++) {
+        let phraseUtilisateur = prompt("Entrez le mot suivant : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+            score++
+        }
+    }
 }
 
-// Idem avec le troisième mot du tableau
-motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score += 1
-    console.log(score)
-} else {
-    console.log("Vous avez fait une erreur de frappe")
-}
-
-// Affichage du score de l'utilisateur
 console.log("Votre score est de " + score + " sur 3")
