9d5ba859c7e0c1169bc48bca39c7334bad03d024
itgbarbara <itgbarbara@gmail.com>
2024-04-29 15:06:23 +0200

    Exercice P2C3 boucles (petites améliorations)

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
 const listeMots = ["Cachalot","Pétunia","Serviette"]
-const listePhrases = ["Pas de panique !","La vie, l'univers et le reste !","Merci pour le poisson"]
+const listePhrases = ["Pas de panique !","La vie, l'univers et le reste ","Merci pour le poisson"]
 
 let score = 0
 
-// Déclaration de la variable contenant le choix de l'utilisateur
 let choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
-//Tant que l'utilisateur n'a pas saisi "Mots" ou "Phrases", on lui redemande de saisir un choix
 while (choix != "Mots" && choix != "Phrases") {
     console.log("Saisie incorrecte")
     choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
 }
 
 if (choix === "Mots") {
-    // On parcourt le tableau des mots
-    for (i=0; i < 3; i++) {
-        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+    for (i=0; i < listeMots.length; i++) {
         let motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[i])
         if (motUtilisateur === listeMots[i]) {
-            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
             score++
         }
     }
+    console.log("Votre score est de " + score + " sur " + listeMots.length")
 } else {
-    for (i=0; i < 3; i++) {
-        let phraseUtilisateur = prompt("Entrez le mot suivant : " + listePhrases[i])
+    for (i=0; i < listePhrases.length; i++) {
+        let phraseUtilisateur = prompt("Entrez la phrase suivante : " + listePhrases[i])
         if (phraseUtilisateur === listePhrases[i]) {
             score++
         }
     }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length")
 }
 
-console.log("Votre score est de " + score + " sur 3")
