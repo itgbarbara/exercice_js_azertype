3fd516447e5fa79cc502082771a3650342130152
itgbarbara <itgbarbara@gmail.com>
2024-04-29 17:13:28 +0200

    Exercice P2C4 fonctions

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-let score = 0
-
-let choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
-while (choix != "Mots" && choix != "Phrases") {
-    console.log("Saisie incorrecte")
-    choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
+function choisirPhrasesOuMots () {
+    let choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
+    while (choix != "Mots" && choix != "Phrases") {
+        console.log("Saisie incorrecte")
+        choix = prompt("Avec quelle liste désirez-vous jouer : 'Mots' ou 'Phrases' ?")
+    }
+    return choix
 }
 
-if (choix === "Mots") {
-    for (i=0; i < listeMots.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot suivant : " + listeMots[i])
-        if (motUtilisateur === listeMots[i]) {
-            score++
+function lancerBoucleDeJeu (listePropositions) {
+        let score = 0
+        for (i=0; i < listePropositions.length; i++) {
+            let motUtilisateur = prompt("Entrez le mot suivant : " + listePropositions[i])
+            if (motUtilisateur === listePropositions[i]) {
+                score++
+            }
         }
-    }
-    console.log("Votre score est de " + score + " sur " + listeMots.length")
-} else {
-    for (i=0; i < listePhrases.length; i++) {
-        let phraseUtilisateur = prompt("Entrez la phrase suivante : " + listePhrases[i])
-        if (phraseUtilisateur === listePhrases[i]) {
-            score++
-        }
-    }
-    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+    return score
 }
 
+function afficherResultat (score, nbPropositions) {
+    console.log("Votre résultat est de " + score + " sur " + nbPropositions)
+}
+
+function lancerJeu () {
+    let choix = choisirPhrasesOuMots ()
+
+    let score = 0
+    let nbPropositions = 0
+
+    if (choix === "Mots") {
+        score = lancerBoucleDeJeu(listeMots)
+        nbPropositions = ListeMots.length
+    } else {
+        score = lancerBoucleDeJeu(ListePhrases)
+        nbPropositions = ListePhrases.length
+    }
+
+    afficherResultat (score, nbPropositions)
+}
+
+lancerJeu()
