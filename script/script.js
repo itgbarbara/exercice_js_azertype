bbc25298bfb594c11b6c1701c2b118dabcd81480
itgbarbara <itgbarbara@gmail.com>
2024-05-01 12:08:51 +0200

    Exercice P3C4 évènements - 1) nettoyage projet

--- script/script.js
+++ script/script.js
@@ -5,36 +5,36 @@
  * 
  *********************************************************************************/
 
-/**
- * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
- * @return {string} : le choix de l'utilisateur, ce choix est nécessairement "mots" ou "phrases
- */
-function choisirPhrasesOuMots() {
-    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    while (choix != "mots" && choix != "phrases") {
-        console.log("Saisie incorrecte")
-        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    }
-    return choix
-}
+// /**
+//  * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
+//  * @return {string} : le choix de l'utilisateur, ce choix est nécessairement "mots" ou "phrases
+//  */
+// function choisirPhrasesOuMots() {
+//     let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+//     while (choix != "mots" && choix != "phrases") {
+//         console.log("Saisie incorrecte")
+//         choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+//     }
+//     return choix
+// }
 
-/**
- * Cette fonction lance la boucle de jeu, c'est à dire qu'elle demande à l'utilisateur de saisir tous les mots
- * contenus dans le tableau listePropositions. A chaque mot saisi, on incrémente le score de l'utilisateur
- * 
- * @param {array[string]} listePropositions 
- * @return {number} : le score de l'utilisateur
- */
-function lancerBoucleDeJeu(listePropositions) {
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            score++
-        }
-    }
-    return score
-}
+// /**
+//  * Cette fonction lance la boucle de jeu, c'est à dire qu'elle demande à l'utilisateur de saisir tous les mots
+//  * contenus dans le tableau listePropositions. A chaque mot saisi, on incrémente le score de l'utilisateur
+//  * 
+//  * @param {array[string]} listePropositions 
+//  * @return {number} : le score de l'utilisateur
+//  */
+// function lancerBoucleDeJeu(listePropositions) {
+//     let score = 0
+//     for (let i = 0; i < listePropositions.length; i++) {
+//         let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+//         if (motUtilisateur === listePropositions[i]) {
+//             score++
+//         }
+//     }
+//     return score
+// }
 
 /**
  * Cette fonction affiche dans la console le score de l'utilisateur
@@ -54,16 +54,16 @@ function afficherResultat(score, nbPropositions) {
  * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
  */
 function lancerJeu() {
-    let choix = choisirPhrasesOuMots()
+    // let choix = choisirPhrasesOuMots()
     let score = 0
     let nbPropositions = 0
 
-    if (choix === "mots") {
-        score = lancerBoucleDeJeu(listeMots)
-        nbPropositions = listeMots.length
-    } else {
-        score = lancerBoucleDeJeu(listePhrases)
-        nbPropositions = listePhrases.length
-    }
+    // if (choix === "mots") {
+    //     score = lancerBoucleDeJeu(listeMots)
+    //     nbPropositions = listeMots.length
+    // } else {
+    //     score = lancerBoucleDeJeu(listePhrases)
+    //     nbPropositions = listePhrases.length
+    // }
     afficherResultat(score, nbPropositions)
 }
